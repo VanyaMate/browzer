3443f00e41bdcddbd1b68954403551be1bddbb3c
Vanya Mate <buhu653@gmail.com>
2023-01-29 06:08:04 +0300

    update users api

--- functions/api/usersApi.js
+++ functions/api/usersApi.js
@@ -13,156 +13,257 @@ const setApi = function (app, db) {
             avatar: './photo.jpg'
         };
     }
-
-    const validationUserData = function (data) {
-        if (!checker.checkLogin(data.login)) {
-            return {error: true, message: "bad login", code: 400};
+    const getUserData = function (userData) {
+        return {
+            ...userData,
+            ...{
+                password: null,
+                sessionId: null
+            }
+        };
+    }
+    const getRequestData = function (requestBody) {
+        return typeof requestBody === 'string' ? JSON.parse(requestBody || "{}") : requestBody;
+    }
+    const checkLoginExist = async function (login) {
+        return (await db.collection('users').doc(login).get()).data();
+    }
+    const validationUserData = function (userData) {
+        if (!checker.checkLogin(userData.login)) {
+            return {error: true, message: 'bad login', code: 400};
         }
 
-        if (!checker.checkPassword(data.password)) {
-            return {error: true, message: "bad password", code: 400};
+        if (!checker.checkPassword(userData.password)) {
+            return {error: true, message: 'bad password', code: 400};
         }
 
-        if (!checker.checkEmail(data.email)) {
-            return {error: true, message: "bad email", code: 400};
+        if (!checker.checkEmail(userData.email)) {
+            return {error: true, message: 'bad email', code: 400};
         }
 
-        if (!checker.checkName(data.personalInfo.firstName)) {
-            return {error: true, message: "bad firstName", code: 400};
+        if (!checker.checkName(userData.personalInfo.firstName)) {
+            return {error: true, message: 'bad firstName', code: 400};
         }
 
-        if (!checker.checkName(data.personalInfo.lastName)) {
-            return {error: true, message: "bad lastName", code: 400};
+        if (!checker.checkName(userData.personalInfo.lastName)) {
+            return {error: true, message: 'bad lastName', code: 400};
         }
 
-        return {error: false, code: 200};
+        return {error: false, data: {}, code: 200};
+    }
+    const validationRequestIP = function (request) {
+        return true;
+    }
+    const validateRequest = function (request) {
+        const data = getRequestData(request.body);
+        const validRequestIP = validationRequestIP(request);
+
+        if (validRequestIP.error) {
+            return {error: true, data: {message: 'bad request'}};
+        } else {
+            return {error: false, data};
+        }
+    }
+    const validationChangeUserData = function (changeUserData) {
+        switch (changeUserData.name) {
+            case 'personalInfo':
+                if (
+                    checker.checkName(changeUserData.value.firstName) &&
+                    checker.checkName(changeUserData.value.lastName)
+                ) {
+                    return true;
+                }
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    const requestHandler = function (req, res, callback) {
+        try {
+            const request = validateRequest(req);
+
+            if (request.error) {
+                res.status(request.code).send({
+                    error: true,
+                    data: {
+                        message: request.data.message
+                    },
+                });
+            } else {
+                callback(request);
+            }
+        }
+        catch (error) {
+            res.status(500).send({
+                error: true,
+                data: error,
+            });
+        }
     }
 
     // Создать пользователя
-    app.post('/api/users', (req, res) => {
-        (async () => {
-            try {
-                const data = typeof req.body === 'string' ? JSON.parse(req.body || "{}") : req.body;
-                const validStatus = validationUserData(data);
+    app.post('/api/users/create', (req, res) => {
+        requestHandler(req, res, async (request) => {
+            const validUserData = validationUserData(request.data);
 
-                if (!validStatus.error) {
-                    let sessionId = null;
-                    let hashPassword = null;
-
-                    await bcrypt.hash(data.login, 10).then((h) => {
-                        sessionId = h;
+            if (validUserData.error) {
+                res.status(200).send({
+                    error: true,
+                    data: {
+                        message: validUserData.message
+                    },
+                });
+                return;
+            } else {
+                if (await checkLoginExist(request.data.login)) {
+                    res.status(200).send({
+                        error: true,
+                        data: {
+                            message: 'login exist'
+                        },
                     });
-                    await bcrypt.hash(data.password, 10).then((h) => {
-                        hashPassword = h;
-                    });
-
-                    await db
-                        .collection('users')
-                        .doc('/' + data.login + '/')
-                        .create({
-                            login: data.login,
-                            password: hashPassword,
-                            personalInfo: {
-                                firstName: {
-                                    value: data.personalInfo.firstName,
-                                    hidden: true
-                                },
-                                lastName: {
-                                    value: data.personalInfo.lastName,
-                                    hidden: true
-                                }
-                            },
-                            avatar: './photo.jpg',
-                            email: data.email,
-                            sessionId: sessionId,
-                        })
-
-                    return res.status(200).send({sessionId, success: true});
-                } else {
-                    return res.status(validStatus.code).send(validStatus.message);
+                    return;
                 }
+
+                let sessionId = null;
+                let hashPassword = null;
+
+                await bcrypt.hash(request.data.login, 10).then((h) => {
+                    sessionId = h;
+                });
+                await bcrypt.hash(request.data.password, 10).then((h) => {
+                    hashPassword = h;
+                });
+
+                const userData = {
+                    login: request.data.login,
+                    password: hashPassword,
+                    personalInfo: {
+                        firstName: {
+                            value: request.data.personalInfo.firstName,
+                            hidden: true
+                        },
+                        lastName: {
+                            value: request.data.personalInfo.lastName,
+                            hidden: true
+                        },
+                    },
+                    avatar: './photo.jpg',
+                    email: request.data.email,
+                    sessionId: sessionId,
+                };
+
+                await db
+                    .collection('users')
+                    .doc('/' + request.data.login + '/')
+                    .create(userData)
+
+                res.status(200).send({
+                    error: false,
+                    data: {
+                        sessionId,
+                        userData: getUserData(userData)
+                    },
+                });
             }
-            catch (error) {
-                console.log(error);
-                return res.status(500).send(error);
-            }
-        })();
+        });
     });
 
     // Получить информацию об одном пользователе по логину
-    app.get('/api/user/:login', (req, res) => {
-        (async () => {
-            try {
-                const document = db.collection('users').doc(req.params.login);
-                const user = await document.get();
-                const response = user.data();
+    app.post('/api/users/getUserByLogin', (req, res) => {
+        requestHandler(req, res, async (request) => {
+            const query = db.collection('users').doc(request.data.login);
+            const user = await query.get();
+            const userData = user.data();
 
-                return res.status(200).send(getPublicData(response));
+            if (userData === undefined) {
+                res.status(200).send({
+                    error: true,
+                    data: {
+                        message: 'No match',
+                    },
+                });
+            } else {
+                res.status(200).send({
+                    error: false,
+                    data: getPublicData(userData)
+                });
             }
-            catch (error) {
-                return res.status(500).send(error);
-            }
-        })();
+        });
     });
 
     // Получить пользователей по совпадению в начале логина
-    app.get('/api/users/:login', (req, res) => {
-        (async () => {
-            try {
-                const document = db.collection('users');
-                const getUsers = document.get();
-                let users = [];
+    app.post('/api/users/getListByLogin', (req, res) => {
+        requestHandler(req, res, async (request) => {
+            const query = db.collection('users')
+                .orderBy('login')
+                .startAt(request.data.login)
+                .limit(request.data.limit);
+            const getUsers = await query.get();
+            const users = getUsers.docs.map((user) => getPublicData(user.data()));
 
-                await getUsers.then(({docs}) => {
-                    const regexp = new RegExp(`^(${req.params.login})`);
-                    for (const doc of docs) {
-                        if (doc.id.match(regexp)) {
-                            users.push(getPublicData(doc.data()));
-                        }
-                    }
-                });
-
-                return res.status(200).send(users);
-            }
-            catch (error) {
-                return res.status(500).send(error);
-            }
-        })();
+            res.status(200).send({
+                error: false,
+                data: users
+            });
+        });
     });
 
     // Изменение информации пользователя
-    app.put('/api/user/:login', (req, res) => {
-        (async () => {
-            const data = typeof req.body === 'string' ? JSON.parse(req.body || "{}") : req.body;
+    app.post('/api/users/change', (req, res) => {
+        requestHandler(req, res, async (request) => {
+            let valid = validationChangeUserData(request.data);
 
-            try {
-                const document = db.collection('users').doc(req.params.login);
+            if (valid && await checkLoginExist(request.data.login)) {
+                const document = db.collection('users').doc(request.data.login);
 
                 await document.update({
-                    [data.changeParamName]: data.changeParamValue
+                    [request.data.name]: request.data.value
                 });
 
-                return res.status(200).send({success: true});
+                res.status(200).send({
+                    error: false,
+                    data: {
+                        message: "changed"
+                    }
+                });
+            } else {
+                res.status(200).send({
+                    error: true,
+                    data: {
+                        message: "bad data"
+                    }
+                });
             }
-            catch (error) {
-                return res.status(500).send(error);
-            }
-        })();
+        });
     });
 
     // Удаление пользователя
-    app.delete('/api/user/:login', (req, res) => {
-        (async () => {
-            try {
-                const document = db.collection('users').doc(req.params.login);
+    app.post('/api/users/delete', (req, res) => {
+        requestHandler(req, res, async (request) => {
+            if (await checkLoginExist(request.data.login)) {
+                const document = db.collection('users').doc(request.data.login);
                 await document.delete();
 
-                return res.status(200).send({success: true});
+                // TODO: Сделать временное удаление с возможностью отмены
+                res.status(200).send({
+                    error: false,
+                    data: {
+                        message: 'init delete',
+                        deleteTime: Date.now() + 1000 * 60 * 60 * 24 * 14,
+                    }
+                });
+            } else {
+                return res.status(200).send({
+                    error: true,
+                    data: {
+                        message: 'bad login',
+                    }
+                });
             }
-            catch (error) {
-                return res.status(500).send(error);
-            }
-        })();
+        });
     });
 }
 
