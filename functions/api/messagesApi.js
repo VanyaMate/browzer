b65dd909fdbe7a851cc496afcbf22cea13319f89
Vanya Mate <buhu653@gmail.com>
2023-01-31 08:08:25 +0300

    update conversation api, add indexes firebase, add offset to conversations

--- functions/api/messagesApi.js
+++ functions/api/messagesApi.js
@@ -1,6 +1,5 @@
 const { requestHandler } = require('../utils/requestMethods').requestMethods;
 const {conversationsApi, loginApi} = require('./api_list').list;
-const { collectionGroup, query, where, getDocs } = require("firebase/firestore");
 
 const setApi = function (app, db) {
     app.post(conversationsApi.get.url, (req, res) => {
@@ -27,24 +26,31 @@ const setApi = function (app, db) {
                         const convQuery = db.collection('conversations').doc(conversation);
                         const convQueryResult = await convQuery.get();
                         const convQueryData = convQueryResult.data();
-                        const messagesQuery = db.collection('messages')
-                            .orderBy('timestamp')
-                            .limit(request.data.limit || 25)
-                        const messagesQueryResult = await messagesQuery.get();
-/*
-                        const messagesQuery = query(
-                            collectionGroup(db, 'messages'),
-                            where('convId', '==', 'Vx5gIwKx3csSWjmum7t3')
-                        );
-                        // // ///
-                        const messagesQueryResult = await getDocs(messagesQuery);*/
-
                         if (convQueryData !== undefined) {
+                            const getMessagesAmount = request.data.limit + request.data.offset;
+                            const messagesQuery = db.collection('messages')
+                                .where('convId', '==', 'Vx5gIwKx3csSWjmum7t3')
+                                .orderBy('timestamp', 'desc')
+                                .limit(getMessagesAmount)
+                            const messagesQueryResult = await messagesQuery.get();
+                            let messages = [];
+
+                            if (request.data.offset === undefined) {
+                                messages = messagesQueryResult.docs.map((result) => result.data()) || []
+                            } else {
+                                for (let i = request.data.offset; i < getMessagesAmount; i++) {
+                                    const message = messagesQueryResult.docs[i];
+                                    if (message) {
+                                        messages.push(message.data());
+                                    }
+                                }
+                            }
+
                             res.status(200).send({
                                 error: false,
                                 data: {
                                     conversation: convQueryData,
-                                    messages: messagesQueryResult.docs.map((result) => result.data()) || []
+                                    messages: messages
                                 }
                             });
                             return;
